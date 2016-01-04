a9ec272cd161ea3589c2ee143d4684e3f74b9b13
chroth7 <chroth@me.com>
2016-01-04 13:43:30 +0100

    Implement regexp to filter.

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -3,13 +3,19 @@ import Map from 'Map';
 
 export default class App extends React.Component {
   render() {
+    const { data } = this.props;
 
-  	let maps = this.props.suffixList.map((suffix)=>
-  		<Map key={suffix} data={this.props.data} suffix={suffix}/>
-  		);
+    const maps = this.props.suffixList.map((suffix)=> {
+        return <Map
+          key={suffix}
+          data={this.props.data}
+          suffix={suffix}
+        />
+      }
+    );
 
     return <div>
-    		{maps}
-    	</div>
+      {maps}
+    </div>
   }
 }
--- src/jsx/Map.jsx
+++ src/jsx/Map.jsx
@@ -4,40 +4,40 @@ const colorScale = d3_scale.viridis();
 
 export default class Map extends React.Component {
   render() {
-
+    const { suffix } = this.props;
     let percentages = {};
     let maxPercent = 0;
     let totalCount = 0;
 
-    this.props.data.forEach((x)=>{
+    const myRegexp = new RegExp(suffix.join('$|') + '$');
+    const filterFunction = str => str.match(myRegexp);
+
+    this.props.data.forEach((x)=> {
       let count = 0;
-      x.forEach((y)=>{
-        if(_.some(this.props.suffix, (s)=>_.endsWith(y.label, s))) {
-          count++;
-        }
-      })
+      count = x.names
+        .filter(filterFunction)
+        .length;
       totalCount += count;
-      percentages[x.id] = count/x.length;
-      // count only bins with at least 20 villages or towns for color scale max
-      if(x.length>20) maxPercent = Math.max(maxPercent, percentages[x.id]);
+      percentages[x.id] = count / x.length;
+      if (x.length > 20) maxPercent = Math.max(maxPercent, percentages[x.id]);
     });
 
-    colorScale.domain([maxPercent, 0])
+    colorScale.domain([maxPercent, 0]);
 
-  	let dots = this.props.data.map((x)=>{
-  		let col = colorScale(percentages[x.id]);
-  		return <circle key={x.id} cx={x.x} cy={x.y} r="2.5" style={{"fill": col}}/>;
-  	});
+    let dots = this.props.data.map((x)=> {
+      let col = colorScale(percentages[x.id]);
+      return <circle key={x.id} cx={x.x} cy={x.y} r="2.5" style={{"fill": col}}/>;
+    });
 
     return (
       <div className="map-tile">
         <h2>-{this.props.suffix[0]}</h2>
-        <h4>{this.props.suffix.slice(1).map((x)=>"-"+x).join(", ")}</h4>
+        <h4>{this.props.suffix.slice(1).map((x)=>"-" + x).join(", ")}</h4>
         <h3>{totalCount} places</h3>
         <svg width="200" height="300">
-      		{dots}
-      	</svg>
-       </div>
+          {dots}
+        </svg>
+      </div>
     );
   }
 }
